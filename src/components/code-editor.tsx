683c3da5fa96405f93053b767dc458a16ec8a210
philippetedajo <philimart21@gmail.com>
2021-03-20 03:08:48 +0100

    update

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,36 +1,64 @@
-import MonacoEditor from "@monaco-editor/react";
+import { useRef } from "react";
+import MonacoEditor, { OnChange, OnMount } from "@monaco-editor/react";
+import prettier from "prettier"
+import parser from "prettier/parser-babel"
+
+//TODO Add format on save
 
 interface codeEditorProps {
   initialValue: string;
-  onChangeCodeInput(value: string): void;
+  onChangeCodeInput(value: string | undefined): void;
 }
 
-const codeEditor: React.FC<codeEditorProps> = ({
+const CodeEditor: React.FC<codeEditorProps> = ({
   initialValue,
   onChangeCodeInput,
 }) => {
-  const handleOnChangeCodeInput = (value: any) => {
+  const codeEditor = useRef<any>();
+
+  const onChange: OnChange = (value) => {
     onChangeCodeInput(value);
   };
 
+  const onMount: OnMount = (monacoEditor) => {
+    codeEditor.current = monacoEditor;
+  };
+
+  const onClick = () => {
+    //formating code on save
+    const unformattedCode = codeEditor.current.getModel().getValue();
+    const formattedCode = prettier.format(unformattedCode,{
+        parser: "babel",
+        plugins: [parser],
+        useTabs: false, 
+        semi: true
+    })
+    codeEditor.current.setValue(formattedCode)
+  };
+
   return (
-    <MonacoEditor
-      value={initialValue}
-      onChange={handleOnChangeCodeInput}
-      language="javascript"
-      theme="vs-dark"
-      height="400px"
-      options={{
-        wordWrap: "on",
-        minimap: {
-          enabled: false,
-        },
-        showUnused: false, // to reset
-        fontSize: 13,
-        automaticLayout: true,
-      }}
-    />
+    <>
+      <button onClick={onClick}>format code</button>
+      <MonacoEditor
+        value={initialValue}
+        onChange={onChange}
+        onMount={onMount}
+        language="javascript"
+        theme="vs-dark"
+        height="400px"
+        options={{
+          wordWrap: "on",
+          minimap: {
+            enabled: false,
+          },
+          showUnused: true, // to reset
+          fontSize: 13,
+          automaticLayout: true,
+          tabSize: 2,
+        }}
+      />
+    </>
   );
 };
 
-export default codeEditor;
+export default CodeEditor;
