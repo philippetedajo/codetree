37fb5d3c4f33edd289da72081fed7ea69bc94ede
Philippe <Philimart21@gmail.com>
2021-03-22 18:40:11 +0100

    update

--- src/components/EditorPreview.tsx
+++ src/components/EditorPreview.tsx
@@ -1,12 +1,51 @@
 import { useRef, useEffect } from "react";
-import { htmlFrameContent } from "../constants";
 import "./styles/code-editor-preview.css";
 
 interface prewiewProps {
   code: string;
+  message: string;
 }
 
-const EditorPreview: React.FC<prewiewProps> = ({ code }) => {
+export const htmlFrameContent = `
+<html>
+  <head>
+    <style>
+      body {
+        background-color: #fff;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="root"></div>
+    <script>
+      const handleError =(error)=>{
+        const root = document.getElementById("root");
+        root.innerHTML = "<div style='color: red'>" + "Runtime error: " + error + "</div>";
+        throw error;
+      }
+
+      window.addEventListener("error", (event) => {
+        event.preventDefault()
+        handleError(event.error)
+      });      
+
+      window.addEventListener(
+        "message",
+        (event) => {
+          try {
+            eval(event.data);
+          } catch (error) {
+            handleError(error)
+          }
+        },
+        false
+      );
+    </script>
+  </body>
+</html>
+`;
+
+const EditorPreview: React.FC<prewiewProps> = ({ code, message }) => {
   const iframe = useRef<any>();
 
   useEffect(() => {
@@ -16,6 +55,8 @@ const EditorPreview: React.FC<prewiewProps> = ({ code }) => {
       iframe.current.contentWindow.postMessage(code, "*");
     }, 50);
   }, [code]);
+
+  console.log(message);
 
   return (
     <div className="preview-wrapper">
@@ -27,6 +68,8 @@ const EditorPreview: React.FC<prewiewProps> = ({ code }) => {
         sandbox="allow-scripts"
         srcDoc={htmlFrameContent}
       />
+
+      {message && <div className="error-message">{message}</div>}
     </div>
   );
 };
