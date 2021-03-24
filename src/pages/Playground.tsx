913cd491f20fe010001a3bcb8471de442f34e7b2
philippetedajo <philimart21@gmail.com>
2021-03-24 21:24:08 +0100

    update

--- src/pages/Playground.tsx
+++ src/pages/Playground.tsx
@@ -1,10 +1,13 @@
 import { Helmet } from "react-helmet";
+import { useLocation } from "react-router-dom";
 import EditorHeader from "../components/EditorHeader";
 import EditorFooter from "../components/EditorFooter";
 import EditorTemplate from "../components/EditorTemplate";
 import "./styles/playground.css";
 
 export const Playground = () => {
+  const location = useLocation();
+  console.log(location);
   return (
     <>
       <Helmet>
