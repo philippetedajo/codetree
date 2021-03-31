ee1d70d9806983938a338236c20d5061ac2ef8b3
Philippe <Philimart21@gmail.com>
2021-03-31 18:27:25 +0100

    reform

--- src/Tree/TreeEditor.tsx
+++ src/Tree/TreeEditor.tsx
@@ -2,40 +2,54 @@ import React from "react";
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
 import { JsTab, JsPanel, HtmlTab, HtmlPanel, CssTab, CssPanel } from "./Pans";
 import SplitBox from "../components/SplitBox";
-import "react-tabs/style/react-tabs.css";
 
 const TreeEditor = () => {
+  let _selectedTab = { bg: "#1B252D", color: "#E5E7EB" };
+  let tabListConfig = {
+    _selected: { bg: "#1B252D", color: "#E5E7EB" },
+    background: "#171E25",
+    color: "#6B7280",
+    pl: 9,
+  };
+  let tabConfig = {
+    _selected: { bg: "#1B252D", color: "#E5E7EB" },
+    className: "font-medium",
+    fontSize: ".92rem",
+  };
+
   return (
     <div style={{ height: "100vh" }}>
       {/* Head ======================================= */}
-      <header style={{ height: "6.5vh" }}>header</header>
+      <header
+        style={{ height: "6.5vh" }}
+        className="flex justify-between items-center editor-header bg-editorprimary text-white border-b-2 border-editorborder px-9"
+      >
+        header
+      </header>
 
       {/* Editor and Preview ======================================= */}
       <SplitBox direction="horizontal">
-        <Tabs
-          style={{ height: "90vh" }}
-          className="h-full border-2 border-red-600 "
-        >
-          <TabList>
-            <Tab>
+        <Tabs style={{ height: "90vh" }}>
+          <TabList {...tabListConfig}>
+            <Tab {...tabConfig}>
               <JsTab />
             </Tab>
-            <Tab>
+            <Tab {...tabConfig}>
               <HtmlTab />
             </Tab>
-            <Tab>
+            <Tab {...tabConfig}>
               <CssTab />
             </Tab>
           </TabList>
 
           <TabPanels className="h-full pt-5">
-            <TabPanel className="h-full">
+            <TabPanel p={0} className="h-full">
               <JsPanel />
             </TabPanel>
-            <TabPanel className="h-full">
+            <TabPanel p={0} className="h-full">
               <HtmlPanel />
             </TabPanel>
-            <TabPanel className="h-full">
+            <TabPanel p={0} className="h-full">
               <CssPanel />
             </TabPanel>
           </TabPanels>
@@ -45,7 +59,12 @@ const TreeEditor = () => {
       </SplitBox>
 
       {/* Foot ======================================= */}
-      <footer style={{ height: "3.5vh" }}>footer</footer>
+      <footer
+        style={{ height: "3.5vh" }}
+        className="editor-footer flex justify-end item-center px-5 text-white text-sm bg-editorprimary border-t-2 border-editorborder"
+      >
+        footer
+      </footer>
     </div>
   );
 };
