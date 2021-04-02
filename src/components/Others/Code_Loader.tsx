175cacd9de972befd3072d873ad2e450caec75bb
philippetedajo <philimart21@gmail.com>
2021-04-02 23:37:46 +0100

    adding loading bundling

---
+++ src/components/Others/Code_Loader.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Code_Loader.css";
+const CodeLoader = () => {
+  return (
+    <span className="loader">
+      <span className="loader-inner" />
+    </span>
+  );
+};
+
+export default CodeLoader;
--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,14 +1,16 @@
 import React, { useRef, useEffect } from "react";
 import { useAppSelector } from "../store/hook";
 import { editor_state } from "../store/features/editorSlice";
+import CodeLoader from "./Others/Code_Loader";
 
 const Preview: React.FC = () => {
   const iframe = useRef<any>();
   const { js, html, css } = useAppSelector(editor_state);
 
   const htmlFrameContent = `
-  <html>
+  <html lang="en">
   <head>
+    <title>CodeTree</title>
     <style>
       ${css.code.data}
     </style>
@@ -43,28 +45,43 @@ const Preview: React.FC = () => {
   `;
 
   useEffect(() => {
-    iframe.current.srcdoc = htmlFrameContent;
+    if (!js.code.loading) {
+      iframe.current.srcdoc = htmlFrameContent;
 
-    setTimeout(() => {
-      iframe.current.contentWindow.postMessage(js.code.data, "*");
-    }, 50);
-  }, [js.code.data, htmlFrameContent]);
+      setTimeout(() => {
+        iframe.current.contentWindow.postMessage(js.code.data, "*");
+      }, 50);
+    }
+  }, [js.code, htmlFrameContent]);
 
   return (
-    <div className="preview-wrapper">
-      <iframe
-        className="preview-iframe"
-        frameBorder="0"
-        ref={iframe}
-        title="previewWindow"
-        sandbox="allow-scripts"
-        srcDoc={htmlFrameContent}
-      />
-
-      {/* <div className="_console">Console</div> */}
-      {/* {message && <div className="error-message">{message}</div>} */}
+    <div className="bg-white">
+      {js.code.loading ? (
+        <div className="flex flex-col items-center justify-start text-white pt-64 fadeInAnim">
+          <CodeLoader />
+          <div className="mt-5">Importing your npm package...</div>
+        </div>
+      ) : (
+        <div className="preview-wrapper">
+          <iframe
+            className="preview-iframe"
+            frameBorder="0"
+            ref={iframe}
+            title="previewWindow"
+            sandbox="allow-scripts"
+            srcDoc={htmlFrameContent}
+          />
+        </div>
+      )}
     </div>
   );
 };
 
 export default Preview;
+
+{
+  /* <div className="_console">Console</div> */
+}
+{
+  /* {message && <div className="error-message">{message}</div>} */
+}
