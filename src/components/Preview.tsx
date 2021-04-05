2d9d719a38f142c4276097277285cdeee6b1496f
philippetedajo <philimart21@gmail.com>
2021-04-06 00:53:40 +0100

    update

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,17 +1,17 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { Resizable } from "re-resizable";
+import { Console, Hook, Unhook } from "console-feed";
 import { useAppDispatch, useAppSelector } from "../store/hook";
-import { add_log, clear_log } from "../store/features/consoleSlice";
 import { editor_state } from "../store/features/editorSlice";
-import { log_state } from "../store/features/consoleSlice";
 
 const Preview: React.FC = () => {
   const iframe = useRef<any>();
   const dispatch = useAppDispatch();
   const { js, html, css } = useAppSelector(editor_state);
-  const { isOpen, logs } = useAppSelector(log_state);
 
-  // console.log(message);
+  const [logs, setLogs] = useState([]);
+
+  console.log(logs);
 
   const htmlFrameContent = `
   <html lang="en">
@@ -25,21 +25,6 @@ const Preview: React.FC = () => {
     ${html.code.data}
     <script>
       //====== send massage to iframe
-      const _log = console.log;
-
-      console.log = function (...rest) {
-        let message = JSON.parse(JSON.stringify(rest))
-        window.parent.postMessage(
-          {
-            source: "iframe",
-            type: "iframe_console_log",
-            message
-          },
-          "*"
-        );
-        _log.apply(console, arguments);
-      };
-
       window.onerror = function (err) {
         window.parent.postMessage(
           { source: "iframe", type: "iframe_error", message: err },
@@ -92,8 +77,10 @@ const Preview: React.FC = () => {
   }, [js.code, htmlFrameContent]);
 
   const clearConsole = () => {
-    dispatch(clear_log());
+    console.log("clearLog");
   };
+
+  let isOpen = false;
 
   return (
     <div className="preview-wrapper">
@@ -102,14 +89,23 @@ const Preview: React.FC = () => {
         frameBorder="0"
         ref={iframe}
         title="previewWindow"
-        sandbox="allow-scripts allow-modals"
+        // sandbox="allow-scripts allow-modals"
         srcDoc={htmlFrameContent}
+        onLoad={() => {
+          Hook(
+            iframe.current.contentWindow.console,
+            (log) => {
+              setLogs((currLogs): any => [...currLogs, log]);
+            },
+            false
+          );
+        }}
       />
       <Resizable
         minWidth="100%"
         minHeight="20vh"
         maxHeight="80vh"
-        defaultSize={{ width: "100%", height: "30vh" }}
+        defaultSize={{ width: "100%", height: "40vh" }}
         className={`console_style ${isOpen ? "hidden" : "flex"} `}
         enable={{
           top: true,
@@ -122,10 +118,27 @@ const Preview: React.FC = () => {
           topLeft: false,
         }}
       >
-        <button onClick={clearConsole}>Clear</button>
+        <Console
+          styles={{
+            BASE_FONT_FAMILY: '"Rubik", sans-serif;',
+            BASE_FONT_SIZE: 14,
+            BASE_BACKGROUND_COLOR: "#171E25",
+          }}
+          logs={logs}
+          variant="dark"
+        />
       </Resizable>
     </div>
   );
 };
 
 export default Preview;
+
+// editor_primary: "#171E25",
+//     editor_secondary: "#1B252D",
+//     editor_border: "#131419",
+//     editor_third: "#4C5B67",
+
+// import React from "react";
+//
+// console.log(React);
