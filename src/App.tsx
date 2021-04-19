6dd1e38fabbf76c1a6daf8110800a0247edb991d
philippetedajo <philimart21@gmail.com>
2021-04-19 23:31:23 +0100

    update meta

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,10 @@ const App = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/Playground" component={TreeEditor} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
+        <Route exact path="/" component={TreeEditor} />
+        {/*<Route path="/Playground" component={TreeEditor} />*/}
+        {/*<Route path="/login" component={Login} />*/}
+        {/*<Route path="/register" component={Register} />*/}
       </Switch>
     </Router>
   );
