7f7f4d3fe0b25387c3f0eaca72b896f4128d9d5b
Philippe <Philimart21@gmail.com>
2021-04-24 15:04:11 +0100

    update preview transition

--- components/others/EditorLoader.tsx
+++ components/others/EditorLoader.tsx
@@ -2,19 +2,21 @@ import React from "react";
 
 const EditorLoader = () => {
   return (
-    <div className="h-screen flex flex-col items-center justify-center">
-      <ul className="loader">
-        <li className="center bg-green-400" />
-        <li className="item item-1 bg-green-400" />
-        <li className="item item-2 bg-green-400" />
-        <li className="item item-3 bg-green-400" />
-        <li className="item item-4 bg-green-400" />
-        <li className="item item-5 bg-green-400" />
-        <li className="item item-6 bg-green-400" />
-        <li className="item item-7 bg-green-400" />
-        <li className="item item-8 bg-green-400" />
-      </ul>
-      <h2 className="text-white mt-7 mr-10">Building stuff ✨ for you.</h2>
+    <div className="h-screen bg-white flex flex-col items-center justify-center border-2 border-red-700 ">
+      <div className="bg-white flex flex-col items-center justify-center transition-all duration-700 fade-loader">
+        <ul className="loader ml-7">
+          <li className="center bg-green-400" />
+          <li className="item item-1 bg-green-400" />
+          <li className="item item-2 bg-green-400" />
+          <li className="item item-3 bg-green-400" />
+          <li className="item item-4 bg-green-400" />
+          <li className="item item-5 bg-green-400" />
+          <li className="item item-6 bg-green-400" />
+          <li className="item item-7 bg-green-400" />
+          <li className="item item-8 bg-green-400" />
+        </ul>
+        <h2 className="text-black mt-7">Building stuff ✨ for you.</h2>
+      </div>
     </div>
   );
 };
