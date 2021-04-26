1324a9800a754a260059142050fb2dc8ce1f2f9f
philippetedajo <philimart21@gmail.com>
2021-04-26 19:51:08 +0100

    update resizable panel

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ const Header: React.FC = () => {
   return (
     <header
       style={{ height: "7vh" }}
-      className="flex tracking-wider font-bold justify-between items-center px-5 bg-tree-soft text-white"
+      className="flex border-b border-black tracking-wider font-bold justify-between items-center px-5 bg-tree-soft text-white"
     >
       <div className="pb-2">
         <span className="text-3xl text-green-500">C</span>odetree
