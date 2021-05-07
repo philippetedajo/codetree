c3f8c92119cef90e514d6a1d4ad5c48daca171da
Philippe <Philimart21@gmail.com>
2021-05-07 11:34:08 +0100

    update purge

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import Head from "next/head";
 import dynamic from "next/dynamic";
 import Split from "react-split";
@@ -7,12 +6,8 @@ import { JsPanel, HtmlPanel, CssPanel } from "../components-editor/Pans";
 import Footer from "../components-editor/Footer";
 import Header from "../components-editor/Header";
 import React from "react";
-import { BiCodeAlt } from "react-icons/bi";
-import { DiCssTricks, DiJavascript1 } from "react-icons/di";
 
 const TreeEditor = () => {
-  const [gutter, setGutter] = useState(false);
-
   const PreviewWithNoSSR = dynamic(
     () => import("../components-editor/Preview"),
     {
@@ -47,7 +42,7 @@ const TreeEditor = () => {
           <div>
             <Tabs
               tabPosition={"top"}
-              tabBarGutter={gutter ? 16 : null}
+              tabBarGutter={16}
               className="programming-language-tabs"
               defaultActiveKey="js"
             >
