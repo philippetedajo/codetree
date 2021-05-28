1aad6dfd59574b5d8ff8e748c23a7fd3c5c8d804
Philippe <Philimart21@gmail.com>
2021-05-28 15:07:32 +0100

    upload profile picture

--- components/layouts/SettingsLayout.tsx
+++ components/layouts/SettingsLayout.tsx
@@ -13,21 +13,19 @@ import ImageUploader from "react-images-upload";
 export const SettingsLayout = ({ children }) => {
   return (
     <div className="pt-4 sm:pt-8 px-3 lg:px-24 flex flex-col sm:flex-row">
-      <nav className="sm:w-56 sm:pt-5">
+      <nav className="sm:w-72 sm:pt-5">
         <ChevronLeftIcon
           onClick={() => Router.push("/profile")}
           className="w-5 h-5 text-3xl mb-5 sm:mb-4 cursor-pointer"
         />
 
-        <div className=" flex flex-row sm:pl-10 sm:flex-col justify-around sm:justify-start ">
+        <div className="flex flex-row sm:pl-10 sm:flex-col justify-around sm:justify-start ">
           <div
-            style={{ height: 120, width: 120 }}
-            className="relative inline-block p-0.5 sm:mb-24 bg-gradient-to-b from-gray-400 to-pink-300 rounded-full mr-5 shadow-lg"
+            style={{ paddingBottom: "1px" }}
+            className="w-52 h-52 relative inline-block sm:mb-24 bg-gradient-to-b from-gray-400 to-pink-300 rounded-full mr-5 shadow-lg"
           >
             <img
-              className="rounded-full"
-              height={120}
-              width={120}
+              className="rounded-full w-52 h-52 border-2 border-red-700"
               src="/fake/profile6.png"
               alt="Profile image"
             />
@@ -37,6 +35,7 @@ export const SettingsLayout = ({ children }) => {
               <CameraIcon className="w-5 h-5 cursor-pointer" />
             </div>
           </div>
+
           <Link href="/settings">
             <a className="flex items-center sm:mb-4">
               <UserCircleIcon className="w-7 h-7 mr-2" />
