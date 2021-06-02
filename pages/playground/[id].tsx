1940a1c63f325ae757d2ed26c98d70c9884caa56
philippetedajo <philimart21@gmail.com>
2021-06-02 22:23:44 +0100

    update

--- pages/playground/[id].tsx
+++ pages/playground/[id].tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Tree from "../../components/editor/Tree";
 import React from "react";
+import { checkSession, withSession } from "../../utils";
 
 const Playground = () => {
   return (
@@ -15,3 +16,11 @@ const Playground = () => {
 };
 
 export default Playground;
+
+export const getServerSideProps = withSession(async ({ req, res }) => {
+  checkSession(req, res);
+
+  return {
+    props: {},
+  };
+});
