7b3f229c1ebce4771c46128b8bbb630151faa707
Philippe <Philimart21@gmail.com>
2021-06-04 19:21:40 +0100

    update

--- components/editor/tools/ErrorScreen.tsx
+++ components/editor/tools/ErrorScreen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export const ErrorScreen = ({ err }) => {
-  return <div className="bg-gray-400 h-full border">{err}</div>;
+  return <div className="h-full text-red-600 p-6">{err}</div>;
 };
