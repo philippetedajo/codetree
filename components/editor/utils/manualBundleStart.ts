a8832a43acb6d19c85fd85fa3741ea9417fbe1fa
philippetedajo <philimart21@gmail.com>
2021-06-13 20:43:36 +0100

    update

--- components/editor/utils/manualBundleStart.ts
+++ components/editor/utils/manualBundleStart.ts
@@ -1,10 +1,24 @@
 import { update_sync_code } from "../../../store/features/editorSlice";
 
 export const manualBundleStart = async (value: any, dispatch) => {
-  dispatch(
+  await dispatch(
     update_sync_code({
       code: value.languages.js.code.data,
       type: "js",
+      error: "",
+    })
+  );
+  await dispatch(
+    update_sync_code({
+      code: value.languages.css.code.data,
+      type: "css",
+      error: "",
+    })
+  );
+  await dispatch(
+    update_sync_code({
+      code: value.languages.html.code.data,
+      type: "html",
       error: "",
     })
   );
