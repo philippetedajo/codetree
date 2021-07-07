d93fa34ba1856e3a754978a1a26bd12b236fe9ac
Philippe <Philimart21@gmail.com>
2021-07-07 15:35:10 +0100

    Feature: Router loader

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,15 @@ import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { store } from "../store/store";
 
+import Router from "next/router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+
 function MyApp({ Component, pageProps }: AppProps) {
+  Router.events.on("routeChangeStart", () => NProgress.start());
+  Router.events.on("routeChangeComplete", () => NProgress.done());
+  Router.events.on("routeChangeError", () => NProgress.done());
+
   return (
     <Provider store={store}>
       <Component {...pageProps} />
