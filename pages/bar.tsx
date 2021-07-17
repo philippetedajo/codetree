108e61f828804642652d48f4231c92b016f48370
phil <philippe.tedajo@gmail.com>
2021-07-17 22:21:05 +0100

    enable ssr

---
+++ pages/bar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Link from "next/link";
+
+const Bar = () => {
+  return (
+    <div>
+      <Link href="/">Go to playground</Link>
+    </div>
+  );
+};
+
+export default Bar;
