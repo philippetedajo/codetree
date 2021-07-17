cbf3e4ecebc5c0ca60f841f6fc563c0928d38d01
phil <philippe.tedajo@gmail.com>
2021-07-17 22:30:47 +0100

    enable ssr on Editor

--- modules/PlaygroundPage.tsx
+++ modules/PlaygroundPage.tsx
@@ -4,6 +4,7 @@ import { SettingsModal } from "../ui/Modals";
 import { compiler_state, initEsbuild } from "../store/features/compilerSlice";
 import { useAppDispatch, useAppSelector } from "../store/hook";
 import { TemplateModal } from "../ui/Modals/TemplateModal";
+
 //Main components
 import EditorInput from "../ui/EditorInput";
 import ConsoleLog from "../ui/ConsoleLog";
