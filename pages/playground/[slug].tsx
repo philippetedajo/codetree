53c1b22a4a06a66593b41fad720b2c7e0b538363
phil <philippe.tedajo@gmail.com>
2021-08-20 23:16:12 +0100

    slug page /project by id

--- pages/playground/[slug].tsx
+++ pages/playground/[slug].tsx
@@ -1,6 +1,27 @@
 import React from "react";
+import { GetServerSideProps } from "next";
+import prisma from "../../libs/prisma";
+import { ProjectProps } from "../../_types/prismaTypes";
 
-const Slug = () => {
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const project = await prisma.project.findUnique({
+    where: {
+      id: Number(query?.key) || -1,
+    },
+  });
+  return {
+    props: {
+      project,
+    },
+  };
+};
+
+type Props = {
+  projects: ProjectProps;
+};
+
+const Slug = (props: Props) => {
+  console.log(props.projects);
   return <div>Playground by id</div>;
 };
 
