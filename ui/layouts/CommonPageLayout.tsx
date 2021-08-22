e71acc1ea66cfe738034fe7c7dc7bba284a0cb25
phil <philippe.tedajo@gmail.com>
2021-08-22 02:41:50 +0100

    upgrade dashboard layout

--- ui/layouts/CommonPageLayout.tsx
+++ ui/layouts/CommonPageLayout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useSession, signOut } from "next-auth/client";
+import Image from "next/image";
 
 export const CommonPageLayout: React.FC = ({ children }) => {
   const router = useRouter();
@@ -10,13 +11,24 @@ export const CommonPageLayout: React.FC = ({ children }) => {
   // @ts-ignore
   return (
     <div className="">
-      <div className="h-14 px-8 md:px-12 flex items-center justify-between">
+      <div className="h-16 px-8 md:px-12 flex items-center justify-between">
         <div>Codetree</div>
         {isLoading ? <div>Loading ...</div> : ""}
         {session && !isLoading ? (
           <div className="flex">
             <Link href="/dashboard">
-              <a className="mr-4">{session?.user?.name}</a>
+              <a className="mr-4 flex items-center">
+                <Image
+                  src={`${session?.user?.image}`}
+                  width={40}
+                  height={40}
+                  className="rounded-full"
+                  alt="avatar"
+                />
+                <span className="ml-3 text-gray-500">
+                  {session?.user?.name}
+                </span>
+              </a>
             </Link>
             <button onClick={() => signOut()}>Logout</button>
           </div>
--- ui/layouts/DashboardLayout.tsx
+++ ui/layouts/DashboardLayout.tsx
@@ -2,13 +2,29 @@ import React from "react";
 import { ActiveLink } from "../ActiveLink";
 import { AiOutlineRadiusSetting } from "react-icons/ai";
 import { ImFilesEmpty } from "react-icons/im";
+import Image from "next/image";
+import { useSession } from "next-auth/client";
+import Link from "next/link";
 
 export const DashboardLayout: React.FC = ({ children }) => {
+  const [session, isLoading] = useSession();
+
   return (
     <div className="flex h-screen">
       <div className="border-r border-black w-2/12 flex flex-col justify-between h-full">
         <div className="h-14 border-b border-black flex items-center px-5">
-          p -- lastname
+          <Link href="/">
+            <a className="mr-4 flex items-center">
+              <Image
+                src={`${session?.user?.image}`}
+                width={40}
+                height={40}
+                className="rounded-full"
+                alt="avatar"
+              />
+              <span className="ml-3 text-gray-500">{session?.user?.name}</span>
+            </a>
+          </Link>
         </div>
         <nav className="flex-1">
           <ul className="mt-5 px-3 flex flex-col space-y-2 text-sm">
