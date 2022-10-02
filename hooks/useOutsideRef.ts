fda74b7911affda45a36996ccfc491ae98a098bc
philippetedajo <philippe.tedajo@gmail.com>
2022-10-02 21:57:51 +0200

    update tailwind

--- hooks/useOutsideRef.ts
+++ hooks/useOutsideRef.ts
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 const UseOutsideRef = (ref: any) => {
   const [isOutsideRef, setIsOutsideRef] = useState(false);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,13 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       animation: {
         "pulse-slow": "pulse 3s linear infinite",
       },
-      width: {
-        112: "30rem",
-      },
-      borderWidth: {
-        3: "3px",
-      },
     },
   },
-  variants: {
-    extend: {
-      animation: ["hover", "focus", "active"],
-      scale: ["active"],
-    },
-  },
-  plugins: [],
 };
